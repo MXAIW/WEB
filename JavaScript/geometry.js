9f57cab9d6737455f731ce22baed93fd559d27d0
MXAIW <marec13mam@gmail.com>
2026-09-24 19:05:34 +0700

    Lesson 9 - Clock in progress

--- JavaScript/geometry.js
+++ JavaScript/geometry.js
@@ -2,21 +2,41 @@
 function figura()
 {
 	let n = Number(document.getElementById('numberEnd').value);
+	let type = Number(document.getElementById('type').value);
 
-	let f = '\n';
+	let f = '';
 	let starsInRow = 1;
-	let currentString = 1;
+	let currentString = 0;
 
-	for (let i = 1; i <= n; i++)
+	if (type === 1 && n != 0)
 	{
-		f += '* ';
-		currentString++;
-		if (currentString == starsInRow)
+		starsInRow = 5;
+		for (let i = 0; i <= n; i++)
 		{
-			f += '\n';
-			starsInRow++;
-			currentString = 0;
+			f += '* ';
+			currentString++;
+			if (currentString == starsInRow)
+			{
+				f += '<br>';
+				currentString = 0;
+			}
 		}
+		document.getElementById('result').innerHTML = `${f}`;
 	}
-	document.getElementById('result').innerHTML = `${f}`;
+	if (type === 2 && n != 0)
+	{
+		let starsInRow = 2;
+		for (let i = 1; i <= n; i++)
+		{
+			f += '* ';
+			currentString++;
+			if (currentString == starsInRow)
+			{
+				f += '<br>';
+				starsInRow++;
+				currentString = 0;
+			}
+		}
+		document.getElementById('result').innerHTML = `${f}`;
+	}
 }
