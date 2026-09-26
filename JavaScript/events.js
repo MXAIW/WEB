1ab1bae8a81720644f878520b5bc9058427ab619
MXAIW <marec13mam@gmail.com>
2026-09-26 18:39:59 +0700

    Lesson 10 - Time of day countdown DONE

--- JavaScript/events.js
+++ JavaScript/events.js
@@ -99,3 +99,65 @@ function tick_timer()
 
 	setTimeout(tick_timer, 100);
 }
+
+document.getElementById("btn-start").addEventListener("click", startCountdownTime)
+function startCountdownTime()
+{
+	let targetDateControl = document.getElementById("target-date");
+	let targetTimeControl = document.getElementById("target-time");
+	let btnStart = document.getElementById("btn-start");
+	if (btnStart.value === "Start")
+	{
+		btnStart.value = "Stop";
+		targetDateControl.disabled = targetTimeControl.disabled = true;
+		tickCountdown()
+	}
+	else
+	{
+		btnStart.value = "Start";
+		targetDateControl.disabled = targetTimeControl.disabled = false;
+	}
+}
+function tickCountdown()
+{
+	let now = new Date();
+	let targetDate = document.getElementById("target-date").valueAsDate;
+	let targetTime = document.getElementById("target-time").valueAsDate;
+
+	//¬ыравниваем часовой по€с
+	targetDate.setHours(targetDate.getHours() + targetDate.getTimezoneOffset() / 60)
+	targetTime.setHours(targetTime.getHours() + targetTime.getTimezoneOffset() / 60)
+
+	//—водим целевые дату и врем€ в одну переменную
+	targetTime.setFullYear(targetDate.getFullYear());
+	targetTime.setMonth(targetDate.getMonth());
+	targetTime.setDate(targetDate.getDate());
+
+	//ќпредел€ем разницу во времени
+	let timestamp = targetTime - now;
+	let duration = Math.trunc(timestamp / 1000);
+
+	document.getElementById("target-date-value").innerHTML = targetDate;
+	document.getElementById("target-time-value").innerHTML = targetTime;
+	document.getElementById("timestamp").innerHTML = timestamp;
+	document.getElementById("duration").innerHTML = duration;
+
+	const SECONDS_PER_MINUTE = 60;
+	const SECONDS_PER_HOUR = 3600;
+	const SECONDS_PER_DAY = 86400;
+	const SECONDS_PER_WEEK = 604800;
+	const DAYS_PER_MONTH = 365.25 / 12;
+	const SECONDS_PER_MONTH = DAYS_PER_MONTH * SECONDS_PER_DAY;
+	const SECONDS_PER_YEAR = SECONDS_PER_DAY * 365 + SECONDS_PER_HOUR * 6;
+
+	//—нова раздел€ем дату и врем€ дл€ удобства вычислени€
+	let time_of_day = duration % SECONDS_PER_DAY;
+	let date = duration - time_of_day;
+
+	document.getElementById("hours-unit").innerHTML = addLeadingZero(Math.trunc(time_of_day / SECONDS_PER_HOUR));
+	time_of_day = time_of_day % SECONDS_PER_HOUR;
+	document.getElementById("minutes-unit").innerHTML = addLeadingZero(Math.trunc(time_of_day / SECONDS_PER_MINUTE));
+	document.getElementById("seconds-unit").innerHTML = addLeadingZero(time_of_day % SECONDS_PER_MINUTE);
+
+	setTimeout(tickCountdown, 100);
+}
